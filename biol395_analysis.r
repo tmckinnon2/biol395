9cda6d0c82242b0b3b1a87846f740a26e5e02796
Tara McKinnon <tmckinno@live.unc.edu>
2016-03-04 14:46:15 -0500

    started reshaping data to ID sites and dates with VF & VFX

--- biol395_analysis.r
+++ biol395_analysis.r
@@ -1,8 +1,26 @@
 # Analysis of experimental exclosure data
 
 # Read in data
-setwd("~Desktop/BIOL 395")
+setwd("~/Desktop/insect-exclosure")
+exclosure <- read.table("exclosure_experiment_2012.txt", sep= '\t', quote="\"", header=TRUE)
+# Remove Unnecessary Columns
+library("dplyr")
+exclosure1 <- select(exclosure, -Strip_length_in)
 
-dat <- read.table("exclosureexperiment1.txt", sep = '\t', quote = "\"", header=TRUE)
+# Subset to data collected in Circle 3, with Visual Surveys, in 2012
+exclosure2 <- filter(exclosure1, circle == "3" & TrapType == "VF")
+exclosure3 <- filter(exclosure2, Year == 2012)
+
+# Calculate arthropod density by Station
+  #New Column with sum arthropod data
+    #ArthropodCount <- select(exclosure3, 14:18)
+    exclosure3$sumarth = rowSums(exclosure[, c('X0.2', 'X2.5')])
+  arthdensity = aggregate(exclosure3$sumarth, b
+                          y = list(Station = exclosure3$Station), 
+                          FUN = mean)
+
+
+
+
   
 
--- data_analysis.R
+++ data_analysis.R
@@ -8,16 +8,44 @@ library("dplyr")
 topdown <- filter(exclosure, Year == 2012)
 topdown1<- select(topdown, -LitterDepth, -TapeExposed_in, -Strip_length_in, -Destroyed., -circle, -Year)
 
-# Add Column with Total # of Arthropods
-topdown1$sumarth = rowSums(topdown1[, c("X0.2mm","X2.5mm","X5.10mm", "X10.20mm", "X.20mm")])
-
 # Add Column combining StateRouteStop and Station
-topdown1$StateRouteStop_Station<- with(topdown1, paste0(StateRouteStop, Station))
+topdown1$StateRouteStop_Station<- paste0(topdown1$StateRouteStop, topdown1$Station)
 
-# Find all rows with an observation in more than 1 size category
-arth0.2 <- filter(topdown1, X0.2mm >0, X2.5mm > 0 | X5.10mm > 0 | X10.20mm > 0 | X.20mm > 0)
-arth2.5 <- filter(topdown1, X2.5mm >0, X5.10mm > 0 | X10.20mm > 0 | X.20mm > 0)
-arth5.10 <- filter(topdown1,  X5.10mm > 0, X10.20mm > 0 | X.20mm > 0)
-arth10.20 <- filter(topdown1,   X10.20mm > 0, X.20mm > 0)
+# Gather Arthropod Size Data into a Single Column
+library(tidyr)
+topdown2 <- gather(topdown1, Size, Abundance, X0.2mm:X.20mm)
+topdown3 <- topdown2[topdown2$Abundance >0, ]
 
+# Identify All Dates for Which There Are VFX Samples 
+trapdates = data.frame(table(topdown3[, c('TrapType', 'Date')]))
+trapdates1 <- filter(trapdates, trapdates$Freq>0)
+trapdates3 <- filter(trapdates1,trapdates1$TrapType == "VFX")
+
+# Subset Full Dataset to those Dates 
+topdown4 <- filter(topdown3, Date == "5/20/2012"|Date == "5/21/2012"|Date == "5/22/2012"
+       |Date == "5/24/2012"|Date == "5/25/2012"|Date == "5/26/2012"|Date == "5/27/2012"
+       |Date == "5/28/2012"|Date == "5/29/2012"|Date == "5/30/2012"|Date == "6/19/2012"
+       |Date == "6/20/2012"|Date == "6/21/2012"|Date == "6/22/2012"|Date == "6/23/2012"
+       |Date == "6/24/2012"|Date == "6/25/2012"|Date == "6/26/2012"|Date == "6/27/2012"
+       |Date == "7/2/2012")
+#Subset Dataset to Only Include Observations at Locations When both VF and VFX are Present 
+
+
+
+#toy example of using gather() from tidyr package
+library(tidyr)
+foo = topdown1[, c(2, 11:16)]
+foo2 = gather(foo, size, abundance, X0.2mm:X.20mm)
+foo3 = foo2[foo2$abundance > 0, ]
+
+dim(foo2)
+dim(foo3)
+gather(topdown1, size, abundance, X0.2mm:X.20mm)
+
+# Counting number of records per category or combination of categories
+table(topdown1$Station)
+data.frame(table(topdown1$Station))
+
+trapcount = data.frame(table(topdown1[, c('TrapType', 'Date',)]))
+trapcount = trapcount[trapcount$Freq > 0,]
 
