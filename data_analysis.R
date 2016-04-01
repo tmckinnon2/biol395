3c1811e5aae395f883bbdcfe669c8085edb143c6
Tara McKinnon <tmckinno@live.unc.edu>
2016-04-01 13:25:35 -0400

    created unique surveys dataframe and added identifiers for summary tables

--- data_analysis.R
+++ data_analysis.R
@@ -3,11 +3,10 @@
 setwd("~/Desktop/insect-exclosure")
 exclosure <- read.table("exclosure_experiment_2012.txt", sep= '\t', quote="\"", header=TRUE)
 
-# Remove Unnecessary Columns
+# Remove Unnecessary Columns 
 library("dplyr")
 topdown <- filter(exclosure, Year == 2012)
 topdown1<- select(topdown, -LitterDepth, -TapeExposed_in, -Strip_length_in, -Destroyed., -circle, -Year)
-
 
 # Add Column combining StateRouteStop and Station
 topdown1$StateRouteStop_Station<- paste0(topdown1$StateRouteStop, topdown1$Station)
@@ -36,30 +35,58 @@ trapdates3 <- filter(trapdates1,trapdates1$TrapType == "VFX")
 topdown5 <- filter(complete_observations, TrapType == "VFX")
 topdown6 <- select(topdown5, StateRouteStop_Station, Date)
 
-#Create a Unique Identifier Column to Filter by Date and Locations in Overall Dataset that Match Those in VFX
+#Create an Identifier Column to Filter by Date and Locations in Overall Dataset that Match Those in VFX
 topdown6$identifier <- paste0(topdown6$StateRouteStop_Station, topdown6$Date)
 complete_observations$identifier <- paste0(complete_observations$StateRouteStop_Station, complete_observations$Date)
 topdown7 <- filter(complete_observations, complete_observations$identifier %in% topdown6$identifier)
 
+#Add unique survey ID
+topdown7$surveyID<- paste0(topdown7$StateRouteStop, topdown7$Date, topdown7$Station, 
+                               topdown7$TrapType)
+
+#Remove Observations with Missing Pair Data
+topdown8 <- topdown7[topdown7$identifier!="82902433B5/22/2012",]
+
 #Include Only Insects Large enough for Bird Consumption
-topdown8 <- filter(topdown7, Size== c("X2.5mm", "X5.10mm", 
+topdown9 <- filter(topdown8, Size== c("X2.5mm", "X5.10mm", 
                    "X10.20mm","X.20mm"))
 
 #Summarise Observations by for All Arthropods
-grouped_all <- topdown8 %>% group_by(TrapType, StateRouteStop, Station, Date)
-mean_abundance_all <- (summarise(grouped_all, mean(Abundance), sd(Abundance)))
-mean_herbivory_all <- (summarise (grouped_all, mean(Herbivory), sd(Herbivory)))
+grouped_all <- topdown9 %>% group_by(TrapType, StateRouteStop, Station, Date)
+mean_abundance_all <- (summarise(grouped_all, mean(Abundance)))
 
 #Summarise Observations for Relevant Orders ("Bird Food Arthropods") 
-food_arthropods <- filter(topdown8, Order == c("LEPL", "COLE", "ARAN", "HETE", "ORTH", "AUCH"))
+food_arthropods <- filter(topdown9, Order == c("LEPL", "COLE", "ARAN", "HETE", "ORTH", "AUCH"))
+
 grouped_food <- food_arthropods %>% group_by(TrapType, StateRouteStop, Station, Date, Order)
-mean_abundance_food <- (summarise(grouped_food, mean(Abundance), sd(Abundance)))
+mean_abundance_food <- (summarise(grouped_food, mean(Abundance)))
 
 #Summarise Observations for Caterpillars
-caterpillars <-filter(topdown7, Order == "LEPL")
+caterpillars <-filter(topdown8, Order == "LEPL")
+grouped_caterpillars <- caterpillars %>% group_by(TrapType, StateRouteStop, Station, Date)
+mean_abundance_caterpillars <- (summarise(grouped_caterpillars, mean(Abundance)))
+mean_abundance_caterpillars$surveyID<- paste0(mean_abundance_caterpillars$StateRouteStop, mean_abundance_caterpillars$Date, 
+                                              mean_abundance_caterpillars$Station, mean_abundance_caterpillars$TrapType)
+
+
+#Create Unique Surveys Dataframe
+unique_surveys<-unique(topdown7[, c("StateRouteStop", "Date", "Station", "TrapType")])
+unique_surveys$surveyID<- paste0(unique_surveys$StateRouteStop, unique_surveys$Date, unique_surveys$Station, 
+                                       unique_surveys$TrapType)
+unique_surveys_count <- data.frame(table(unique_surveys[, c("StateRouteStop", "Date", "Station", "TrapType", "surveyID")]))
+unique_surveys_count = unique_surveys_count[unique_surveys_count$Freq> 0,]
+
+
+caterpillars1 <- merge(unique_surveys_count, caterpillars, by.x="surveyID", by.y = "surveyID")
 
 #Code to Look at Data Errors
 dataframename$TrapType [df$RecordID %in% c(10631,10632,10633)]
+
+#Dr. Hurlbert's Code for Finding Frequency of Plant species
+stationplants = unique(topdown1[, c('StateRouteStop', 'Station', 'TrapType', 'Date', 'TreeSpecies')])
+plantspCount = data.frame(table(stationplants[, c('StateRouteStop', 'Station', 'TrapType', 'TreeSpecies')]))
+plantspCount = plantspCount[plantspCount$Freq > 0,]
+plantspCount = plantspCount[order(plantspCount$StateRouteStop, plantspCount$Station, plantspCount$TrapType),]
 
 
 
