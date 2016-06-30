cfd8f7f2e9301077a273c58578d66dcec1190b96
Tara McKinnon <tmckinno@live.unc.edu>
2016-06-30 15:37:49 -0400

    started using 395 code to group and 2016 data into arthropod groups of interest

--- ExclosureAnalysis_Summer2016.R
+++ ExclosureAnalysis_Summer2016.R
@@ -1,3 +1,4 @@
+##Need to incorporate something into script to remove beat sheet surveys
 #Open necessary packages
 library(dplyr)
 # Read in data
@@ -52,7 +53,9 @@ ex_pairs <-filter(ex_pairs_alldates, grepl("5/11/16", timeStart) |
                                      grepl("6/23/16", timeStart) | 
                                      grepl("6/24/16", timeStart))
 #Add column marking exclosures
-ex_pairs1 <- merge(ex_pairs, exclosures, by.x = "orderID", by.y= "orderID", all.x = TRUE)
+ex_pairs1 <- merge(ex_pairs, exclosures, by.x = "orderID", 
+                                         by.y= "orderID",
+                                         all.x = TRUE)
 ex_pairs2 <- select(ex_pairs1, -c(21:38), -identifier.y)
 ex_pairs2[is.na(ex_pairs2)] <- "no"
 names(ex_pairs2) <- c("OrderID", "surveyID", "siteID", "userID", "circle",
@@ -60,48 +63,117 @@ names(ex_pairs2) <- c("OrderID", "surveyID", "siteID", "userID", "circle",
                       "siteNotes", "plantSpecies", "herbivory", "isValid",
                       "surveyType", "leafCount", "orderArthropod", "orderLength",
                       "orderNotes", "orderCount", "identifier", "exclosure")
-if (grepl("5/11/16",ex_pairs2$timeStart)) {ex_pairs2$date = "5/11/16"}
-    else {if (grepl("5/12/16",ex_pairs2$timeStart)) {ex_pairs2$date<-"5/12/16"
-    } else {if (grepl("5/16/16",ex_pairs2$timeStart)) {ex_pairs2$date<-"5/16/16"
-    } else {if (grepl("5/18/16",ex_pairs2$timeStart)) {ex_pairs2$date<-"5/18/16"
-    } else {if (grepl("6/23/16",ex_pairs2$timeStart)) {ex_pairs2$date<-"6/23/16"
-    } else {if (grepl("6/24/16",ex_pairs2$timeStart)) {ex_pairs2$date<-"6/24/16"
-    } else ex_pairs2$date <- "NA"}}}}} 
 
+#Add dates column ***not a neat method, need to get "ifelse" to work***
+May11 <- filter(ex_pairs2, grepl("5/11/16", timeStart))
+May11$date <- "5/11/16"
 
-ex_pairs2$date <- (ifelse((grepl("5/11/16",ex_pairs2$timeStart)), "5/11/16", "NA")
-                         ifelse((grepl("5/12/16",ex_pairs2$timeStart)), "5/12/16", "NA")
-                         ifelse((grepl("5/16/16",ex_pairs2$timeStart)), "5/16/16", "NA")
-                         ifelse((grepl("5/18/16",ex_pairs2$timeStart)), "5/18/16", "NA")
-                         ifelse((grepl("6/23/16",ex_pairs2$timeStart)), "6/23/16", "NA")
-                         ifelse((grepl("6/24/16",ex_pairs2$timeStart)), "6/24/16", "NA"))
+May12 <- filter(ex_pairs2, grepl("5/12/16", timeStart))
+May12$date <- "5/12/16"
+
+May16 <- filter(ex_pairs2, grepl("5/16/16", timeStart))
+May16$date <- "5/16/16"
+
+May18 <- filter(ex_pairs2, grepl("5/18/16", timeStart))
+May18$date <- "5/18/16"
+
+June23 <- filter(ex_pairs2, grepl("6/23/16", timeStart))
+June23$date <- "6/23/16"
+
+June24 <- filter(ex_pairs2, grepl("6/24/16", timeStart))
+June24$date <- "6/24/16"
+
+ex_pairs3 <- bind_rows(May11, May12, May16, 
+                       May18, June23, June24)
+
 #Subset into NCBG and PR DataFrames
-PR<- filter(ex_pairs2, siteID == "117")
-NCBG <- filter(ex_pairs2, siteID == "8892356")
+PR<- filter(ex_pairs3, siteID == "117")
+NCBG <- filter(ex_pairs3, siteID == "8892356")
 
 
-
-
-#Summarise observations for 3 food types
+#Summarise observations for 3 food types (start 2012 reshaping analysis code)
 #PR
 #Summarise Observations for All Arthropods (Arthropods greater than 2 mm)
-grouped_all <- PR %>% group_by(TrapType, StateRouteStop, Station, VisitNumber)
-total_all <- (summarise(grouped_all, sum(Abundance)))
-total_all$surveyID<- paste0(total_all$StateRouteStop, total_all$VisitNumber,
-                            total_all$Station, total_all$TrapType)
+grouped_all_PR <- PR %>% group_by(exclosure, siteID, circle, survey, date)
+total_all_PR <- (summarise(grouped_all_PR, sum(orderCount)))
+total_all_PR$surveyID<- paste0(total_all_PR$siteID, total_all_PR$date,
+                            total_all_PR$circle, total_all_PR$survey,
+                            total_all_PR$exclosure)
 
 
 #Summarise Observations for Relevant Orders ("Bird food Arthropods") 
-food_arthropods <- filter(topdown10, Order %in% c("LEPL", "COLE", "ARAN", "HETE", "ORTH", "AUCH"))
-grouped_food <- food_arthropods %>% group_by(TrapType, StateRouteStop, Station, VisitNumber)
-total_food <- (summarise(grouped_food, sum(Abundance)))
-total_food$surveyID<- paste0(total_food$StateRouteStop, total_food$VisitNumber, 
-                             total_food$Station, total_food$TrapType)
+food_arthropods_PR <- filter(PR, orderArthropod %in% c("Caterpillars (Lepidoptera larvae)", 
+                                                       "Beetles (Coleoptera)", 
+                                                       "Spiders (Araneae; NOT daddy longlegs!)", 
+                                                       "True Bugs (Heteroptera)", 
+                                                       "Grasshoppers, Crickets (Orthoptera)", 
+                                                       "Leaf hoppers and Cicadas (Auchenorrhyncha)"))
+grouped_food_PR <- food_arthropods_PR %>% group_by(exclosure, siteID, circle, survey, date)
+total_food_PR <- (summarise(grouped_food_PR, sum(orderCount)))
+total_food_PR$surveyID<- paste0(total_food_PR$siteID, total_food_PR$date,
+                                total_food_PR$circle, total_food_PR$survey,
+                                total_food_PR$exclosure)
 
 #Summarise Observations for Caterpillars
-caterpillars <-filter(topdown9, Order == "LEPL")
-grouped_caterpillars <- caterpillars %>% group_by(TrapType, StateRouteStop, Station, VisitNumber)
-total_caterpillars <- (summarise(grouped_caterpillars, sum(Abundance)))
-total_caterpillars$surveyID<- paste0(total_caterpillars$StateRouteStop, total_caterpillars$VisitNumber, 
-                                     total_caterpillars$Station, total_caterpillars$TrapType)
+caterpillars_PR <-filter(PR, orderArthropod == "Caterpillars (Lepidoptera larvae)")
+grouped_caterpillars_PR <- caterpillars_PR %>% group_by(exclosure, siteID, circle, survey, date)
+total_caterpillars_PR <- (summarise(grouped_caterpillars_PR, sum(orderCount)))
+total_caterpillars_PR$surveyID<- paste0(total_caterpillars_PR$siteID, total_caterpillars_PR$date,
+                                total_caterpillars_PR$circle, total_caterpillars_PR$survey,
+                                total_caterpillars_PR$exclosure)
 
+#Create Unique Surveys Dataframe
+unique_surveys_PR<-unique(PR[, c("exclosure", "siteID", "circle", "survey", "date")])
+unique_surveys_PR$surveyID<- paste0(unique_surveys_PR$siteID, unique_surveys_PR$date, 
+                                    unique_surveys_PR$circle, unique_surveys_PR$survey,
+                                    unique_surveys_PR$exclosure)
+unique_surveys_count_PR <- data.frame(table(unique_surveys_PR[, c("exclosure", 
+                                                               "siteID", 
+                                                               "circle", 
+                                                               "survey", 
+                                                               "date", "surveyID")]))
+unique_surveys_count_PR = unique_surveys_count_PR[unique_surveys_count_PR$Freq> 0,]
+
+#Create 3 New Dataframes Merging List of Unique Surveys with Summary Observations 
+#for Each Unique Survey for Each of the 3 Food Types
+all_abundance_PR <- merge(unique_surveys_count_PR, total_all_PR,
+                                                   by.x="surveyID",
+                                                   by.y = "surveyID", 
+                                                   all.x = TRUE)
+all_abundance1_PR<- select(all_abundance_PR, -Freq,
+                                             -exclosure.y, 
+                                             -siteID.y, 
+                                             -circle.y, 
+                                             -survey.y 
+                                             -date.y, 
+                                             -surveyID)
+names(all_abundance1) <- c('siteID','date', "Station", "TrapType","total_all")
+all_abundance1[is.na(all_abundance1)] <- 0
+
+food_abundance <- merge(unique_surveys_count, total_food, by.x="surveyID", by.y = "surveyID", all.x = TRUE)
+food_abundance1<- select(food_abundance, -Freq,-TrapType.y, -StateRouteStop.y, -Station.y, -VisitNumber.y, -surveyID)
+names(food_abundance1) = c('StateRouteStop','VisitNumber', "Station", "TrapType","total_food")
+food_abundance1[is.na(food_abundance1)] <- 0
+
+caterpillar_abundance <- merge(unique_surveys_count, total_caterpillars, by.x="surveyID", by.y = "surveyID", all.x = TRUE)
+caterpillar_abundance1<- select(caterpillar_abundance, -Freq,-TrapType.y, -StateRouteStop.y, -Station.y, -VisitNumber.y, -surveyID)
+names(caterpillar_abundance1) = c('StateRouteStop','VisitNumber', "Station", "TrapType","total_caterpillars")
+caterpillar_abundance1[is.na(caterpillar_abundance1)] <- 0
+
+
+#*****Attempts at "ifelse" code to revisit
+if (grepl("5/11/16",ex_pairs2$timeStart)) {ex_pairs2$date = "5/11/16"}
+else {if (grepl("5/12/16",ex_pairs2$timeStart)) {ex_pairs2$date<-"5/12/16"
+} else {if (grepl("5/16/16",ex_pairs2$timeStart)) {ex_pairs2$date<-"5/16/16"
+} else {if (grepl("5/18/16",ex_pairs2$timeStart)) {ex_pairs2$date<-"5/18/16"
+} else {if (grepl("6/23/16",ex_pairs2$timeStart)) {ex_pairs2$date<-"6/23/16"
+} else {if (grepl("6/24/16",ex_pairs2$timeStart)) {ex_pairs2$date<-"6/24/16"
+} else ex_pairs2$date <- "NA"}}}}} 
+
+
+ex_pairs2$date <- (ifelse((grepl("5/11/16",ex_pairs2$timeStart)), "5/11/16", "NA")
+                   ifelse((grepl("5/12/16",ex_pairs2$timeStart)), "5/12/16", "NA")
+                   ifelse((grepl("5/16/16",ex_pairs2$timeStart)), "5/16/16", "NA")
+                   ifelse((grepl("5/18/16",ex_pairs2$timeStart)), "5/18/16", "NA")
+                   ifelse((grepl("6/23/16",ex_pairs2$timeStart)), "6/23/16", "NA")
+                   ifelse((grepl("6/24/16",ex_pairs2$timeStart)), "6/24/16", "NA"))
