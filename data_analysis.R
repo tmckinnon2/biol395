2a3386c1aa710fad6317d32aed0aa31e8158b30f
Tara McKinnon <tmckinno@live.unc.edu>
2016-09-16 13:21:05 -0400

    minor changes for sourcing to exclosures modeling

--- data_analysis.R
+++ data_analysis.R
@@ -1,40 +1,47 @@
-# Analysis of experimental exclosure data
+# Analysis of experimental exclosure data from Southern Appalachians 2012
+
+#Load Required Packages
+library(dplyr)
+library(tidyr)
+library(coin)
+library(lattice)
+library(maps)
+
 # Read in data
 setwd("~/Desktop/insect-exclosure")
 topdown <- read.table("exclosure_experiment_2012_TM_corrected.txt", sep= '\t', quote="\"", header=TRUE)
 
 # Remove Unnecessary Columns 
-library("dplyr")
 topdown1<- select(topdown, -LitterDepth, -TapeExposed_in, -Strip_length_in, -circle, -Year)
 
 # Add Column combining StateRouteStop and Station
 topdown1$StateRouteStop_Station<- paste0(topdown1$StateRouteStop, topdown1$Station)
 
 # Gather Arthropod Size Data into a Single Column
-library(tidyr)
 topdown2 <- gather(topdown1, Size, Abundance, X0.2mm:X.20mm)
 topdown3 <- topdown2[topdown2$Abundance >0, ]
 
 #Add Back "0" Observations from Original Data
 ##Identify "0" Observations from Original Data
-no_observations <- filter(topdown1, X0.2mm==0 & X2.5mm==0 & X5.10mm==0 & X10.20mm==0 & X.20mm==0)
+no_observations <- dplyr::filter(topdown1, X0.2mm==0 & X2.5mm==0 & X5.10mm==0 & X10.20mm==0 & X.20mm==0)
 ##Format "0" Observations to Match topdown3
 no_observations1 <- select(no_observations, -X0.2mm, -X2.5mm, -X5.10mm, -X10.20mm, -X.20mm)
 no_observations1$Abundance = 0
 no_observations1$Size = NA
 class(no_observations1$Size)="character"
 class(no_observations1$Abundance)="integer"
+
 ##Add Back "0" Observations to topdown3 to Create Complete Dataset
-complete_observations <- union(no_observations1, topdown3)
+complete_observations <- dplyr::union(no_observations1, topdown3)
 
 #Create List of Surveys with  VFX Entries 
-topdown5 <- filter(complete_observations, TrapType == "VFX")
+topdown5 <- dplyr::filter(complete_observations, TrapType == "VFX")
 topdown6 <- select(topdown5, StateRouteStop_Station, Date)
 
-#Create an Identifier Column to Filter by Date and Locations in Overall Dataset that Match Those in VFX
+#Create an Identifier Column to filter by Date and Locations in Overall Dataset that Match Those in VFX
 topdown6$identifier <- paste0(topdown6$StateRouteStop_Station, topdown6$Date)
 complete_observations$identifier <- paste0(complete_observations$StateRouteStop_Station, complete_observations$Date)
-topdown7 <- filter(complete_observations, complete_observations$identifier %in% topdown6$identifier)
+topdown7 <- dplyr::filter(complete_observations, complete_observations$identifier %in% topdown6$identifier)
 
 #Add Unique Survey ID
 topdown7$surveyID<- paste0(topdown7$StateRouteStop, topdown7$Date, topdown7$Station, 
@@ -42,7 +49,7 @@ topdown7$surveyID<- paste0(topdown7$StateRouteStop, topdown7$Date, topdown7$Stat
 
 # Narrow down to StateRouteStop-stations with Complete Sampling in Both Visits 1 and 3
 ##Filter to Visits 1 and 3, when both VFX and VF surveys were conducted
-topdown8 <- filter(topdown7, VisitNumber %in% c(1,3))
+topdown8 <- dplyr::filter(topdown7, VisitNumber %in% c(1,3))
 ##Identify all unique StateRouteStop Station Visits
 uniqSiteVisits <- unique(topdown8[, c('StateRouteStop', 'Station', 'TrapType', 'VisitNumber')])
 ##Identify Stations for which there are 4 Unique Visits- signifies that data is present in both 
@@ -54,27 +61,27 @@ goodstations$StateRouteStop_Station <- paste(goodstations$StateRouteStop, goodst
 topdown9 <- topdown8[topdown8$StateRouteStop_Station %in% goodstations$StateRouteStop_Station, ]
 
 #Include Only Insects Large enough for Bird Consumption
-topdown10 <- filter(topdown9, Size %in% c("X5.10mm", 
+topdown10 <- dplyr::filter(topdown9, Size %in% c("X5.10mm", 
                                          "X10.20mm","X.20mm"))
 #Summarise Observations for 3 Food Types
 #Summarise Observations for All Arthropods (Arthropods greater than 5 mm)
 grouped_all <- topdown10 %>% group_by(TrapType, StateRouteStop, Station, VisitNumber)
-total_all <- (summarise(grouped_all, sum(Abundance)))
+total_all <- (dplyr::summarise(grouped_all, sum(Abundance)))
 total_all$surveyID<- paste0(total_all$StateRouteStop, total_all$VisitNumber,
                                        total_all$Station, total_all$TrapType)
 
 
 #Summarise Observations for Relevant Orders ("Bird food Arthropods") 
-food_arthropods <- filter(topdown10, Order %in% c("LEPL", "COLE", "ARAN", "HETE", "ORTH", "AUCH"))
+food_arthropods <- dplyr::filter(topdown10, Order %in% c("LEPL", "COLE", "ARAN", "HETE", "ORTH", "AUCH"))
 grouped_food <- food_arthropods %>% group_by(TrapType, StateRouteStop, Station, VisitNumber)
-total_food <- (summarise(grouped_food, sum(Abundance)))
+total_food <- (dplyr::summarise(grouped_food, sum(Abundance)))
 total_food$surveyID<- paste0(total_food$StateRouteStop, total_food$VisitNumber, 
                                               total_food$Station, total_food$TrapType)
 
 #Summarise Observations for Caterpillars
-caterpillars <-filter(topdown9, Order == "LEPL")
+caterpillars <-dplyr::filter(topdown9, Order == "LEPL")
 grouped_caterpillars <- caterpillars %>% group_by(TrapType, StateRouteStop, Station, VisitNumber)
-total_caterpillars <- (summarise(grouped_caterpillars, sum(Abundance)))
+total_caterpillars <- (dplyr::summarise(grouped_caterpillars, sum(Abundance)))
 total_caterpillars$surveyID<- paste0(total_caterpillars$StateRouteStop, total_caterpillars$VisitNumber, 
                                               total_caterpillars$Station, total_caterpillars$TrapType)
 
@@ -123,7 +130,6 @@ names(caterpillar_time) = c('StateRouteStop','Station', "TrapType", "Visit1", "V
 caterpillar_time$visit_dif<-caterpillar_time$Visit3-caterpillar_time$Visit1
 
 #Run wilcox_test
-library("coin")
 wilcox_test(visit_dif ~ TrapType, data=all_time)
 wilcox_test(visit_dif ~ TrapType, data=food_time)
 wilcox_test(visit_dif ~ TrapType, data=caterpillar_time)
@@ -136,24 +142,24 @@ plantspCount <- data.frame(table(stationplants[, c('StateRouteStop', 'Station', 
 plantspCount <- plantspCount[plantspCount$Freq > 0,]
 plantspCount <- plantspCount[order(plantspCount$StateRouteStop, plantspCount$Station, plantspCount$TrapType),]
 ##Identify Combinations with Freq=2 (tree species for a treatment within a station is the same for both visits) 
-plantspCount1 <- filter(plantspCount, Freq==2) 
+plantspCount1 <- dplyr::filter(plantspCount, Freq==2) 
 plantspCount2 <- spread(plantspCount1, key=TrapType, value=TreeSpecies)
 ##Identify Combinations with the Same Tree Species for VFX and VF
-plantspCount3 <- filter(plantspCount2, VF==VFX)
+plantspCount3 <- dplyr::filter(plantspCount2, VF==VFX)
 plantspCount3$ID <- paste(plantspCount3$StateRouteStop, plantspCount3$Station)
 ##Subset Existing Food Type Datasets to Create 3 New Food Type Datasets with Only Observations from Stations 
 ##Where VFX and VF are the Same ("Paired Stations")
 paired_all <- all_time
 paired_all$ID <- paste(paired_all$StateRouteStop, paired_all$Station)
-paired_all1 <- filter(paired_all, paired_all$ID %in% plantspCount3$ID)
+paired_all1 <- dplyr::filter(paired_all, paired_all$ID %in% plantspCount3$ID)
 
 paired_food <- food_time
 paired_food$ID <- paste(paired_food$StateRouteStop, paired_food$Station)
-paired_food1 <- filter(paired_food, paired_food$ID %in% plantspCount3$ID)
+paired_food1 <- dplyr::filter(paired_food, paired_food$ID %in% plantspCount3$ID)
 
 paired_caterpillar <- caterpillar_time
 paired_caterpillar$ID <- paste(paired_caterpillar$StateRouteStop, paired_caterpillar$Station)
-paired_caterpillar1 <- filter(paired_caterpillar, paired_caterpillar$ID %in% plantspCount3$ID)
+paired_caterpillar1 <- dplyr::filter(paired_caterpillar, paired_caterpillar$ID %in% plantspCount3$ID)
 
 #Run Wilcox Test on Paired Stations Data for 3 Food Types
 wilcox_test(visit_dif ~ TrapType, data=paired_all1)
@@ -288,7 +294,7 @@ caterpillar_avg <- summarize(caterpillar_time4, mean(VFX_VF_dif))
 
 ###Graphing
 #Example Change in Abundance Over Time Graph (for one StateRouteStop-Station)
-ex_graph_data <-filter(all_abundance2, StateRouteStop == "8890236", Station== "3B")
+ex_graph_data <-dplyr::filter(all_abundance2, StateRouteStop == "8890236", Station== "3B")
 ex_graph_data$VisitNumber <- as.numeric(as.character(ex_graph_data$VisitNumber))
 plot(x=ex_graph_data$VisitNumber, y=ex_graph_data$VFX, xlim=range(c(0,4)), ylim=range(c(1,5)), 
             xlab="Visit Number", ylab="Arthropod Abundance", type="b", col="red", main="Sample Change In Arthropod Abundance Over Time")
@@ -297,7 +303,6 @@ points(x=ex_graph_data$VisitNumber, y=ex_graph_data$VF, type="b", col="blue")
 
 #Histogram for All 3 Food Types 
 #Showing the difference between change in VFX over time and change in VF over time
-library(lattice)
 histogram(all_time4$VFX_VF_dif, breaks=30, xlim=c(-20,15), 
           xlab="Difference in Change in Arth Density between VFX and VF",
           main="All Arthropods >2.0 mm")
@@ -314,23 +319,23 @@ histogram(caterpillar_time4$VFX_VF_dif, breaks=10, ylab="Percent of Total",
 ##Read in BBS Lat-Long Data
 lat_longs <- read.table("BBS_stop_latlongs.txt", sep= '\t', quote="\"", header=TRUE)
 ##Filter data to only include StateRouteStop locations where the exclosure experiment occurred
-exclosure_lat_longs<- filter(lat_longs, Stateroutestop %in% c("2704132", "6302205", "6303117", 
+exclosure_lat_longs<- dplyr::filter(lat_longs, Stateroutestop %in% c("2704132", "6302205", "6303117", 
                                                               "6390627", "6390644", "6390909", 
                                                               "6390944", "6391108", "8204219", 
                                                               "8290243", "8290339", "8290344", 
                                                               "8890009", "8890029", "8890236"))
 ##Map lat-long data
-library("maps")
-map('state', xlim = c(-85, -75), ylim = c(32, 40))
-points(exclosure_lat_longs$Longitude,exclosure_lat_longs$Latitude, pch = 16, col ='green', 
-       cex = 1.5, main="Survey Site Locations")
+map('state', xlim = c(-85, -75), ylim = c(33, 40))
+points(exclosure_lat_longs$Longitude,exclosure_lat_longs$Latitude, pch = 4, col ='green', 
+       cex = 1, bg="black", main="Survey Site Locations")
+
 ##Trying out analyzing by tree species
-uniqTree <- unique(topdown9$TreeSpecies)
-uniqTrees <-as.character(uniqTree)
-topdown9$TreeSpecies <- as.character(topdown9$TreeSpecies)
-trees= c()
-for (tree in uniqTrees) {
-  print(filter(topdown9, TreeSpecies == tree))}
-treeSp= data.frame(trees)
+#uniqTree <- unique(topdown9$TreeSpecies)
+#uniqTrees <-as.character(uniqTree)
+#topdown9$TreeSpecies <- as.character(topdown9$TreeSpecies)
+#trees= c()
+#for (tree in uniqTrees) {
+#  print(dplyr::filter(topdown9, TreeSpecies == tree))}
+#treeSp= data.frame(trees)
 
 
